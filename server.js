2557a44d8f61a765c7b6d8a7bd3f416f789af22f
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-07-09 14:22:35 -0500

    a little but of front end

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ db.once('open', function() {
   console.log('mongoose is connected')
 });
 
+
+var newsSchema = mongoose.Schema({
+    name: String
+  });
+
 // //not gonna Use mongojs to hook the database to the db variable
 // var db = mongoose(databaseUrl, collections);
 
@@ -38,7 +43,64 @@ db.once('open', function() {
 //     console.log("Database Error:", error);
 // });
 
-//Routes
+
+
+
+// Routes
+// 1. At the root path, send a simple hello world message to the browser
 app.get("/", function(req, res) {
-    res.send("This will one day get an article");
-});
+    res.send("Hello world");
+  });
+  
+  // 2. At the "/all" path, display every entry in the animals collection
+  app.get("/all", function(req, res) {
+    // Query: In our database, go to the animals collection, then "find" everything
+    db.animals.find({}, function(error, found) {
+      // Log any errors if the server encounters one
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        res.json(found);
+      }
+    });
+  });
+  
+  // 3. At the "/name" path, display every entry in the animals collection, sorted by name
+  app.get("/name", function(req, res) {
+    // Query: In our database, go to the animals collection, then "find" everything,
+    // but this time, sort it by name (1 means ascending order)
+    db.animals.find().sort({ name: 1 }, function(error, found) {
+      // Log any errors if the server encounters one
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        res.json(found);
+      }
+    });
+  });
+  
+  // 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
+  app.get("/weight", function(req, res) {
+    // Query: In our database, go to the animals collection, then "find" everything,
+    // but this time, sort it by weight (-1 means descending order)
+    db.animals.find().sort({ weight: -1 }, function(error, found) {
+      // Log any errors if the server encounters one
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        res.json(found);
+      }
+    });
+  });
+  
+  // Set the app to listen on port 3000
+  app.listen(3000, function() {
+    console.log("App running on port 3000!");
+  });
+  
